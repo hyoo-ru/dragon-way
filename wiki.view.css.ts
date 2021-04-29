ec7f504186fc95d2c006d481ddb797bd53cac5de
jin <jin@hyoo.ru>
2021-04-29 14:35:26 +0300

    Single page with local storage synchronization

---
+++ wiki.view.css.ts
@@ -0,0 +1,11 @@
+namespace $ {
+	
+	$mol_style_define( $my_wiki, {
+		
+		Body: {
+			padding: $mol_gap.block,
+		},
+		
+	} )
+	
+}
---
+++ wiki.view.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+	
+	export class $my_wiki extends $.$my_wiki {
+		
+		text( next?: string ) {
+			return this.$.$mol_state_local.value( 'text', next ) ?? ''
+		}
+		
+	}
+	
+}
