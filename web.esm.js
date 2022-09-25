e58fa3e624ed2230495f95693f1c5247a87c4db6
nin-jin <nin-jin@users.noreply.github.com>
2022-09-25 07:13:20 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -9578,6 +9578,9 @@ var $;
         enabled() {
             return true;
         }
+        spellcheck() {
+            return true;
+        }
         length_max() {
             return +Infinity;
         }
@@ -9591,6 +9594,7 @@ var $;
             obj.value = (val) => this.value(val);
             obj.hint = () => this.hint();
             obj.enabled = () => this.enabled();
+            obj.spellcheck = () => this.spellcheck();
             obj.length_max = () => this.length_max();
             obj.selection = (val) => this.selection(val);
             return obj;
