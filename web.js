959d374364293629c3d629b1c56006312bf07906
nin-jin <nin-jin@users.noreply.github.com>
2025-03-08 07:05:47 +0000

    Deploying to gh-pages from @ hyoo-ru/dragon-way@60eaf1d422cfdcef07c8e3daa86671fdf7569360 🚀

--- web.js
+++ web.js
@@ -10423,6 +10423,7 @@ var $;
         'https://crus.hyoo.ru/',
         'https://i-love-crus.ru/',
         'https://crus.xedth.ru/',
+        'https://crus.87-120-36-150.ip.hyoo.ru/',
     ];
 })($ || ($ = {}));
 
