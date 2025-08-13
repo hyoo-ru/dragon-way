0adb09f3ecfd6dd2e51fb979d77aa8bf93e0b924
nin-jin <nin-jin@users.noreply.github.com>
2025-08-13 07:12:55 +0000

    Deploying to gh-pages from @ hyoo-ru/dragon-way@60eaf1d422cfdcef07c8e3daa86671fdf7569360 🚀

--- node.d.ts
+++ node.d.ts
@@ -2312,7 +2312,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $mol_crypto_sacred extends $mol_buffer {
+    type BufferSource = ArrayBufferView<ArrayBuffer> | ArrayBuffer;
+    export class $mol_crypto_sacred extends $mol_buffer {
         static size: 16;
         static make(): $mol_crypto_sacred;
         static from<This extends typeof $mol_buffer>(this: This, serial: string | ArrayBufferView<ArrayBuffer>): InstanceType<This>;
@@ -2330,6 +2331,7 @@ declare namespace $ {
         close(sacred: DataView<ArrayBuffer>, salt: BufferSource): Promise<Uint8Array<ArrayBuffer>>;
         open(buf: Uint8Array<ArrayBuffer>, salt: BufferSource): Promise<Uint8Array<ArrayBuffer>>;
     }
+    export {};
 }
 
 declare namespace $ {
